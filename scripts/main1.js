f4337fcdd795c1ef19c99019d8fbd42b6d38be73
bandura <banduraweb@ukr.net>
2019-11-05 23:34:30 +0200

    Updates

--- scripts/main1.js
+++ scripts/main1.js
@@ -1,19 +1,27 @@
 'use strict';
 
 (() => {
+  const id = setInterval(function () {
+    step = step === 4 ? 1 : step = step + 1;
+    sliderContainer.style.cssText
+      = `background: url("https://banduraweb.github.io/layout_dia/images/slider/slide-img-${step}.jpg");`;
+  }, 3000);
+
   const buttonPrev = document.getElementById('next');
   const buttonNext = document.getElementById('prev');
   const sliderContainer = document.querySelector('.header__slider');
 
   let step = 1;
 
-  buttonNext.onclick = function() {
+  buttonNext.onclick = function () {
+    clearInterval(id);
     step = step === 4 ? 1 : step = step + 1;
     sliderContainer.style.cssText
       = `background: url("https://banduraweb.github.io/layout_dia/images/slider/slide-img-${step}.jpg");`;
   };
 
-  buttonPrev.onclick = function() {
+  buttonPrev.onclick = function () {
+    clearInterval(id);
     step = step === 1 ? step = 4 : step = step - 1;
     sliderContainer.style.cssText
       = `background: url("https://banduraweb.github.io/layout_dia/images/slider/slide-img-${step}.jpg");`;
