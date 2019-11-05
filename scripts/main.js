64296c568cbe230e993c80c00d3a22885dc618f1
bandura <banduraweb@ukr.net>
2019-11-05 11:33:07 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -5,23 +5,25 @@
   const sliderContainer = document.querySelector('.header__slider');
 
   let step = 1;
-  let id = setInterval(function () {
+  const id = setInterval(function() {
     step = step === 4 ? 1 : step = step + 1;
     sliderContainer.style.cssText
       = `background: url("../images/slider/slide-img-${step}.jpg");`;
   }, 3000);
 
-  buttonNext.onclick = function () {
+  buttonNext.onclick = function() {
     clearInterval(id);
     step = step === 4 ? 1 : step = step + 1;
     sliderContainer.style.cssText
-      = `transition: none; background: url("../images/slider/slide-img-${step}.jpg");`;
+      = `transition: none; background: 
+      url("../images/slider/slide-img-${step}.jpg");`;
   };
 
-  buttonPrev.onclick = function () {
+  buttonPrev.onclick = function() {
     clearInterval(id);
     step = step === 1 ? step = 4 : step = step - 1;
     sliderContainer.style.cssText
-      = `transition: none; background: url("../images/slider/slide-img-${step}.jpg");`;
+      = `transition: none; background: 
+      url("../images/slider/slide-img-${step}.jpg");`;
   };
 })();
