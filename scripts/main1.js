7dee8185fd570f1c4bcfe4982b39f3b5879927d8
bandura <banduraweb@ukr.net>
2019-11-08 18:35:38 +0200

    Updates

--- scripts/main1.js
+++ scripts/main1.js
@@ -1,35 +1,3 @@
-// (() => {
-//   const sliderInner = document.querySelector('.slider__inner');
-//   const buttonPrev = document.getElementById('next');
-//   const buttonNext = document.getElementById('prev');
-//
-//   let step = 0;
-//   const imgWidth = 100;
-//   const maxWidth = 400;
-//
-//   const id = setInterval(function() {
-//     step = step - imgWidth;
-//     if (step === -maxWidth) {
-//       step = 0;
-//     }
-//     sliderInner.style.cssText = `left:${step}%`;
-//   }, 4000);
-//
-//   buttonNext.onclick = function() {
-//     clearInterval(id);
-//     step = step === -(maxWidth - imgWidth) ? step = 0 : step = step - imgWidth;
-//     sliderInner.style.cssText = `left:${step}%`;
-//   };
-//
-//   buttonPrev.onclick = function() {
-//     clearInterval(id);
-//     step = step === 0 ? step = -(maxWidth - imgWidth) : step = step + imgWidth;
-//     sliderInner.style.cssText = `left:${step}%`;
-//   };
-//
-// })();
-
-//
 'use strict';
 
 const sliderInner = document.querySelector('.slider__inner');
@@ -45,7 +13,6 @@ function currentWidth() {
 }
 
 document.body.onresize = function () {
-  // location.reload();
   let newWidth = document.body.clientWidth;
   let st = 0;
   setSize(newWidth, st);
@@ -55,31 +22,25 @@ document.body.onresize = function () {
 function setSize(w, s) {
   s = 0;
   for (let i = 0; i < images.length; i++) {
-    images[i].style.cssText = `width: ${w / 1.4}px; height: ${w * 0.56}px;`;
+    images[i].style.cssText = `width: ${w / 1.4}px; height: ${w * 0.4}px;`;
   }
-  imagesContainer.style.cssText = `width: ${w / 1.4}px; height: ${w * 0.56}px;`;
+  imagesContainer.style.cssText = `width: ${w / 1.4}px; height: ${w * 0.4}px;`;
   let step = s;
   const imgWidth = 100;
   const maxWidth = 400;
 
-  // const id = setInterval(function () {
-  //   step = step - imgWidth;
-  //   if (step === -maxWidth) {
-  //     step = 0;
-  //   }
-  //   sliderInner.style.cssText = `left:${step}%`;
-  // }, 4000);
 
   buttonNext.onclick = nextClick;
+
   function nextClick() {
-    //clearInterval(id);
+
     step = step === -(maxWidth - imgWidth) ? step = 0 : step = step - imgWidth;
     sliderInner.style.cssText = `left:${step}%`;
   }
 
 
   buttonPrev.onclick = function () {
-    //clearInterval(id);
+
     step = step === 0 ? step = -(maxWidth - imgWidth) : step = step + imgWidth;
     sliderInner.style.cssText = `left:${step}%`;
 
