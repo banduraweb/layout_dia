fb85e0af54dde92314d2c0aa341ffe6fb54ff3d2
bandura <banduraweb@ukr.net>
2019-11-10 19:21:11 +0200

    Updates

---
+++ scripts/lib/modules/main.js
@@ -0,0 +1,13 @@
+
+import {onReady} from './mobil-menu.js'
+import * as lib from './main1.js';
+onReady();
+setSize(currentWidth);
+
+//console.log(onReady);
+// http://localhost:8080/scripts/lib/modules/mobil-menu
+// http://localhost:8080/scripts/lib/modules/mobil-menu.js
+
+//import myDefault, * as myModule from '/modules/my-module.js';
+
+//import myDefault, {foo, bar} from '/modules/my-module.js';
--- scripts/mobil-menu.js
+++ scripts/mobil-menu.js
@@ -1,22 +1,37 @@
-'use strict';
+"use strict";
+
 document.addEventListener("DOMContentLoaded", onReady);
 
 function onReady() {
-  const buttonOpen = document.getElementById('mobile__btn--open');
-  const buttonClose = document.getElementById('mobile__btn--close');
-  const navMenu = document.getElementById('nav__list');
+  console.log(currentWidth());
+  const buttonOpen = document.getElementById("mobile__btn--open");
+  const buttonClose = document.getElementById("mobile__btn--close");
+  const navMenu = document.getElementsByClassName("nav__list")[0];
 
+  window.addEventListener("resize", showHideMobBtn);
 
-  buttonOpen.onclick = function() {
-    navMenu.style.cssText = 'display: flex;';
-    buttonOpen.style.cssText = 'display: none;';
-    buttonClose.style.cssText = 'display: block;';
-  };
-  buttonClose.onclick = function() {
-    navMenu.style.cssText = 'display: none;';
-    buttonClose.style.cssText = 'display: none;';
-    buttonOpen.style.cssText = 'display: flex;';
+  function showHideMobBtn() {
 
+    if (document.body.clientWidth > 1181) {
+      navMenu.style.cssText = "display: flex;";
+      buttonOpen.style.cssText = "display: none;";
+      buttonClose.style.cssText = "display: none;";
+    } else {
+      navMenu.style.cssText = "display: none;";
+      buttonOpen.style.cssText = "display: block;";
+      buttonClose.style.cssText = "display: none;";
+    }
   }
+
+  buttonOpen.onclick = () => {
+    navMenu.style.cssText = "display: flex;";
+    buttonOpen.style.cssText = "display: none;";
+    buttonClose.style.cssText = "display: block;";
+  };
+  buttonClose.onclick = () => {
+    navMenu.style.cssText = "display: none;";
+    buttonClose.style.cssText = "display: none;";
+    buttonOpen.style.cssText = "display: flex;";
+  };
 
 }
